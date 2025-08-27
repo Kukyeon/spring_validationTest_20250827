d4cd27ded3af56ebd11843d8a383d553df0773f8
Kukyeon <rnrdus2000@naver.com>
2025-08-27 16:40:53 +0900

    lombok

---
+++ src/main/java/com/kkuk/member/dto/MemberDto.java
@@ -0,0 +1,22 @@
+package com.kkuk.member.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class MemberDto {
+
+	private String id; // 회원아이디
+	private String password; // 비밀번호
+	private String confirmPassword; //비밀번호 확인
+	private String name;
+	private String email; //회원이메일
+	private int age; // 나이 
+
+	
+}
